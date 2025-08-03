3c22d993c02abf37caa45d14d712f26dccf0f463
Gaurav Aryal <44746573+gaurav-aryal@users.noreply.github.com>
2025-08-03 00:25:44 -0600

    Add simple login view

--- EisenhowerMatrixApp/EisenhowerMatrixAppApp.swift
+++ EisenhowerMatrixApp/EisenhowerMatrixAppApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct EisenhowerMatrixAppApp: App {
+    @State private var currentUser: String?
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if let user = currentUser {
+                ContentView(taskManager: TaskManager(userId: user))
+            } else {
+                LoginView { username in
+                    currentUser = username
+                }
+            }
         }
     }
 }
